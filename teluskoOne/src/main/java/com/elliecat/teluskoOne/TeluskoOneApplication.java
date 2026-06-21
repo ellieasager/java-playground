1c6982d79dd6781172d25e8263d4894f7ed4198e
Ellie A Sager <34947031+ellieasager@users.noreply.github.com>
2026-06-20 19:02:59 -0700

    project from youtube

---
+++ teluskoOne/src/main/java/com/elliecat/teluskoOne/TeluskoOneApplication.java
@@ -0,0 +1,13 @@
+package com.elliecat.teluskoOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluskoOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluskoOneApplication.class, args);
+	}
+
+}
